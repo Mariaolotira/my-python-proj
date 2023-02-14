b0fe1a3bbb301f1fc6b731b421cf750e4f97eee0
eMobilis <sandrasasimwa@gmail.com>
2023-02-14 12:53:37 +0300

    redone fizzbuzz

--- quiz/main.py
+++ quiz/main.py
@@ -1,11 +1,12 @@
-#number=int(input("Enter a number from 1 to 100"))
-for number in range(100):
-    if number % 3 ==0 and number % 5==0:
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+for number in range(num1, num2):
+    if number % 3 == 0 and number % 5 == 0:
         print(number, "fizzBuzz")
-    elif number % 3 ==0:
+    elif number % 3 == 0:
         print(number, "fizz")
-    elif number % 5 ==0:
+    elif number % 5 == 0:
         print(number, "Buzz")
-
-
-
+    else:
+        print(number)
